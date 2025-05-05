e4a34fda703dad71bdd0a7ab576f465cc584ed64
Shishir Pathak <seaseeraw12@gmail.com>
2025-05-05 07:53:19 +0545

    first

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbars from './components/Navbar';
+
+const App = () => {
+  return (
+    <div>
+      <Navbars/>
+    </div>
+  )
+}
+
+export default App;
